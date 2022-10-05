50749dea6d734afbd4c2f5191efb0395825ee999
Samiwel Thomas <samiwel.thomas@digital.homeoffice.gov.uk>
2022-10-05 12:33:05 +0100

    Fix counters not displaying.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const nunjucks= require('nunjucks');
+const bodyParser = require('body-parser');
+
+const nunjucks = require('nunjucks');
 const promClient = require('prom-client');
+const {last} = require("nunjucks/src/filters");
 
 const PORT = 8081;
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 nunjucks.configure('views', {
-	autoescape: true,
-	express: app
+    autoescape: true,
+    express: app
 });
 
 // Create a Registry which registers the metrics
@@ -15,56 +19,76 @@ const register = new promClient.Registry();
 
 // Add a default label which is added to all metrics
 register.setDefaultLabels({
-  app: 'demo',
-	team: "sre",
+    app: 'demo',
+    team: "sre",
 });
 
-promClient.collectDefaultMetrics({ register });
+promClient.collectDefaultMetrics({
+    register,
+})
 
 const successful = new promClient.Counter({
-  name: 'simple_counter1',
-	help: 'A demo counter metric',
-	status: 'SUCCESS',
+    name: 'simple_counter1',
+    help: 'A demo counter metric',
+    status: 'SUCCESS',
 });
-
-successful.inc()
 
 const exceptions = new promClient.Counter({
-  name: 'simple_counter2',
-	help: 'A demo counter metric',
-	status: 'EXCEPTION',
+    name: 'simple_counter2',
+    help: 'A demo counter metric',
+    status: 'EXCEPTION',
 });
 
-exceptions.inc()
+register.registerMetric(successful);
+register.registerMetric(exceptions);
 
-function getTotal() {
-	return successful.Counter + exceptions.Counter;
+const getCounterValue = async name => {
+    const theGood = await register.getSingleMetricAsString(name);
+    const matches = theGood.match(/(\d+)$/);
+    const good = matches.length > 1 ? Number.parseInt(matches[matches.length - 1], 10) : 0;
+    return Promise.resolve(good);
+}
+
+
+const getLastSuccessValue = async () => {
+    return getCounterValue("simple_counter1");
+}
+
+const getLastExceptionValue = async () => {
+    return getCounterValue("simple_counter2");
 }
 
 app.get('/metrics', async (req, res) => {
-  res.setHeader('Content-Type', register.contentType);
-  res.send(await register.metrics());
+    res.setHeader('Content-Type', register.contentType);
+    res.send(await register.metrics());
 });
 
-app.get('/', (req, res) => {
-	console.log(successful);
-	console.log(exceptions);
-  res.render('index.html', {successful: successful, exceptions: exceptions, total: getTotal()});
+app.get('/', async (req, res) => {
+    const good = await getLastSuccessValue();
+    const bad = await getLastExceptionValue();
+    console.log({
+        good, bad
+    })
+
+    const total = good + bad;
+    res.render('index.html', {successful: good, exceptions: bad, total});
 });
 
-app.post('/success', (req, res) =>{
-	console.log("In success");
-	successful.inc();
-	res.redirect('/');
+app.post('/success', (req, res) => {
+    const inc = Number.parseInt(req.body.amount, 10);
+    console.log("In success");
+    successful.inc(inc);
+    res.redirect('/');
 });
 
-app.post('/exception', (req ,res) =>{
-	console.log("In exception");
-	exceptions.inc();
-	res.redirect('/');
+app.post('/exception', (req, res) => {
+    const inc = Number.parseInt(req.body.amount, 10);
+    console.log("In exception");
+    exceptions.inc(inc);
+    res.redirect('/');
 });
 
 app.listen(PORT, () => {
-  console.log(`sre-demo-nodejs-app listening on port: http://localhost:${PORT}`);
+    console.log(`sre-demo-nodejs-app listening on port: http://localhost:${PORT}`);
 });
 
